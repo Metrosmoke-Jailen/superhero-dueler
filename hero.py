1cf107b220d2366b3517e0238c1c41672e584386
Jailen-Truitt <jailen.johnson-truitt@students.dominican.edu>
2026-02-19 12:09:10 -0800

    updated

--- hero.py
+++ hero.py
@@ -1,3 +1,5 @@
+import random
+
 class Hero:
   def __init__(self, name, starting_health=100):
     '''Instance properties:
@@ -10,7 +12,24 @@ class Hero:
     self.starting_health = starting_health
     self.current_health = starting_health
 
+  def fight(self, opponent):
+        '''Current Hero will take turns fighting the opponent hero passed in.'''
+
+        winner = random.choice([self, opponent])
+        loser = opponent if winner == self else self
+
+        print(f"{winner.name} defeats {loser.name}!")
+        return winner
+
+
 if __name__ == "__main__":
   my_hero = Hero("Grace Hopper", 200)
   print(my_hero.name)
   print(my_hero.current_health)
+
+  print("\n--- Battle Test ---")
+
+  hero1 = Hero("Wonder Woman")
+  hero2 = Hero("Dumbledore")
+
+  hero1.fight(hero2)
